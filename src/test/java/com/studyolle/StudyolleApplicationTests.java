0813c83c4741b545a39d70aae7899950b0c0a7f1
muezwink <muezwink@gmail.com>
2020-06-07 15:10:00 +0900

    project init, Account.java add

---
+++ src/test/java/com/studyolle/StudyolleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleApplicationTests
+{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
